c349e477ea236011d2bb4f53e1d8e91a79a8e163
njall skarphedinsson <njallis@gmail.com>
2023-04-19 10:01:31 -0400

    adding config files

---
+++ app.config.js
@@ -0,0 +1,44 @@
+const VERSION = "1.0.0";
+const ANDROID_BULD_NO = 1;
+
+export default {
+	slug: "GameQA",
+	extra: {
+		eas: {
+			projectId: "4122cc65-2b51-44d8-868e-f0e753814f79",
+		},
+	},
+	ios: {
+		supportsTablet: true,
+		googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
+		bundleIdentifier: "com.spurningaris.gameqaen",
+		buildNumber: VERSION,
+	},
+	android: {
+		adaptiveIcon: {
+			foregroundImage: "./assets/adaptive-icon.png",
+			backgroundColor: "#FFA800",
+		},
+		googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+		bundleIdentifier: "com.gameqa.gameqa-en",
+		permissions: [],
+		versionCode: ANDROID_BULD_NO,
+		config: {
+			branch: {
+				apiKey: "key_live_kkWb728Kh7lmtcY9KFBT9hdfwrlTN6eD",
+			},
+		},
+	},
+	version: VERSION,
+	orientation: "portrait",
+	icon: "./assets/icon.png",
+	splash: {
+		image: "./assets/splash.png",
+		resizeMode: "contain",
+		backgroundColor: "#8950fc",
+	},
+	updates: {
+		fallbackToCacheTimeout: 0,
+	},
+	assetBundlePatterns: ["*/"],
+};
